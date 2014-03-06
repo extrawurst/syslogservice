cfb07309fb1163a93398147a02c1ab135253282a
Stephan Dilly <stephan@extrawurst.org>
2014-03-06 17:19:28 +0100

    converted service to a class to be able to use it as a library too

--- source/app.d
+++ source/app.d
@@ -2,124 +2,30 @@ module app;
 
 import vibe.d;
 
-import std.stdio;
-
-ushort port = 8888;
-string hostName = "hostUnknown";
-string logFolder = "./";
-bool quiet = true;
+import syslogservice;
 
 shared static this()
 {
+	ushort port = 8888;
+	string hostName = "hostUnknown";
+	string logFolder = "./";
+	bool quiet = true;
+
 	getOption("folder",&logFolder,"log folder");
 	getOption("hostport",&port,"port");
 	getOption("hostname",&hostName,"hostname");
 	getOption("quiet",&quiet,"no logging");
 
-	auto settings = new HTTPServerSettings();
-	settings.port = port;
-	settings.options = HTTPServerOption.parseFormBody;
-	settings.bindAddresses = ["0.0.0.0"];
+	auto logger = new SysLogService();
+	logger.port = port;
+	logger.hostName = hostName;
+	logger.logFolder = logFolder;
+	logger.quiet = quiet;
 
-	listenHTTP(settings, &handleRequest);
+	logger.start();
 
 	logInfo("hostname: %s",hostName);
 	logInfo("quiet: %s",quiet);
 	logInfo("logfolder: '%s'",logFolder);
 	logInfo(" ");
-}
-
-void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.statusCode = 200;
-	res.writeBody("");
-
-	auto lastSlash = req.requestURL.lastIndexOf('/');
-	if(lastSlash == -1)
-	{
-		logError("req has no event set: %s",req.requestURL);
-		return;
-	}
-
-	auto event = req.requestURL[lastSlash+1..$];
-
-	if(event.length == 0)
-	{
-		logError("req has no event set: %s",req.requestURL);
-		return;
-	}
-
-	syslog(event, req.form, req.peer, req.clientAddress.port, req.headers["user-agent"]);
-}
-
-void syslog(string _event, FormFields _values, string _ip, ushort _port, string _userAgent)
-{
-	auto logline = createSyslogLine(_event, _values);
-
-	if(!quiet)
-		logInfo("%s",logline);
-
-	appendToFile(logFolder ~ getLogFileName(),logline);
-}
-
-string getLogFileName()
-{
-	auto currentTime = Clock.currTime();
-
-	return format("%04d-%02d-%02d_%s.log",
-		currentTime.year,
-		currentTime.month,
-		currentTime.day,
-		hostName);
-}
-
-string getLogLineDate()
-{
-	auto currentTime = Clock.currTime();
-
-	enum monthNames = [ 
-		"Jan", "Feb", "Mar", "Apr", "May", "Jun", 
-		"Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
-
-	//Sep 11 00:02:14
-	return format("%s %02d %02d:%02d:%02d",
-				  monthNames[currentTime.month-1],
-				  currentTime.day,
-				  currentTime.hour,
-				  currentTime.minute,
-				  currentTime.second);
-}
-
-string createSyslogLine(string _event, FormFields _values)
-{
-	import std.array;
-
-	Appender!string line;
-
-	line.put(getLogLineDate());
-
-	line ~= " "; 
-	line ~= hostName;
-
-	line ~= " ";
-	line ~= _event;
-
-	if(_values.length > 0)
-	{
-		line ~= " - [";
-		
-		foreach(k, v; _values)
-		{
-			line ~= k;
-			line ~= "=\"";
-			line ~= v; 
-			line ~= "\" ";
-		}
-
-		line ~= "]";
-	}
-
-	line ~= "\n";
-
-	return line.data;
 }
---
+++ source/syslogservice.d
@@ -0,0 +1,143 @@
+﻿module syslogservice;
+
+import vibe.d;
+
+///
+final class SysLogService
+{
+private:
+	///
+	public alias RequestCallback = void delegate(string,HTTPServerRequest);
+
+	bool m_quiet;
+	ushort m_port = 8888;
+	string m_hostName = "hostUnknown";
+	string m_logFolder = "./";
+	RequestCallback m_requestModifierCallback;
+
+	///
+	@property public void port(ushort _port) { m_port = _port; }
+	///
+	@property public void quiet(bool _quiet) { m_quiet = _quiet; }
+	///
+	@property public void hostName(string _hostname) { m_hostName = _hostname; }
+	///
+	@property public void logFolder(string _logFolder) { m_logFolder = _logFolder; }
+	///
+	@property public void requestModifierCallback(RequestCallback _cb) { m_requestModifierCallback = _cb; }
+
+	/++ 
+	 + starts the http server.
+	 + If not changed the default listens on port 8888, 
+	 + is quiet (does not print every log msg to the stdout), 
+	 + is named "hostUnknown" and 
+	 + writes the logfiles to "./".
+	 +/
+	public void start()
+	{
+		auto settings = new HTTPServerSettings();
+		settings.port = m_port;
+		settings.options = HTTPServerOption.parseFormBody;
+		settings.bindAddresses = ["0.0.0.0"];
+		
+		listenHTTP(settings, &handleRequest);
+	}
+
+	void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
+	{
+		res.statusCode = 200;
+		res.writeBody("");
+		
+		auto lastSlash = req.requestURL.lastIndexOf('/');
+		if(lastSlash == -1)
+		{
+			logError("req has no event set: %s",req.requestURL);
+			return;
+		}
+		
+		auto event = req.requestURL[lastSlash+1..$];
+		
+		if(event.length == 0)
+		{
+			logError("req has no event set: %s",req.requestURL);
+			return;
+		}
+
+		if(m_requestModifierCallback)
+			m_requestModifierCallback(event, req);
+		
+		syslog(event, req.form, req.peer, req.clientAddress.port, req.headers["user-agent"]);
+	}
+
+	void syslog(string _event, FormFields _values, string _ip, ushort _port, string _userAgent)
+	{
+		auto logline = createSyslogLine(_event, _values);
+		
+		if(!m_quiet)
+			logInfo("%s",logline);
+		
+		appendToFile(m_logFolder ~ getLogFileName(),logline);
+	}
+	
+	string getLogFileName()
+	{
+		auto currentTime = Clock.currTime();
+		
+		return format("%04d-%02d-%02d_%s.log",
+		              currentTime.year,
+		              currentTime.month,
+		              currentTime.day,
+		              m_hostName);
+	}
+	
+	string getLogLineDate()
+	{
+		auto currentTime = Clock.currTime();
+		
+		enum monthNames = [ 
+			"Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+			"Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+		
+		//Sep 11 00:02:14
+		return format("%s %02d %02d:%02d:%02d",
+		              monthNames[currentTime.month-1],
+		currentTime.day,
+		currentTime.hour,
+		currentTime.minute,
+		currentTime.second);
+	}
+	
+	string createSyslogLine(string _event, FormFields _values)
+	{
+		import std.array;
+		
+		Appender!string line;
+		
+		line.put(getLogLineDate());
+		
+		line ~= " "; 
+		line ~= m_hostName;
+		
+		line ~= " ";
+		line ~= _event;
+		
+		if(_values.length > 0)
+		{
+			line ~= " - [";
+			
+			foreach(k, v; _values)
+			{
+				line ~= k;
+				line ~= "=\"";
+				line ~= v; 
+				line ~= "\" ";
+			}
+			
+			line ~= "]";
+		}
+		
+		line ~= "\n";
+		
+		return line.data;
+	}
+}
