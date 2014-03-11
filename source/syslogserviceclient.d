f6cd6a179c377cad608a3c5adc26d8a6b0638a84
Stephan Dilly <stephan@extrawurst.org>
2014-03-11 18:02:44 +0100

    support parameters in syslog client

--- source/syslogserviceclient.d
+++ source/syslogserviceclient.d
@@ -1,5 +1,8 @@
 ﻿module syslogserviceclient;
 
+import vibe.core.core:runTask;
+import vibe.http.client;
+
 ///
 final class SyslogServiceClient
 {
@@ -14,10 +17,28 @@ public:
 	}
 
 	///
+	void log(string _event)(in string[string] params)
+	{
+		import vibe.http.form;
+
+		if(url.length == 0)
+			return;
+
+		runTask({
+			requestHTTP(url~_event,
+			            (scope HTTPClientRequest req) {
+				req.method = HTTPMethod.POST;
+				
+				req.writeFormBody(params);
+			});
+		});
+	}
+
+	///
 	void log(string _event)()
 	{
-		import vibe.core.core:runTask;
-		import vibe.http.client:requestHTTP,HTTPClientRequest,HTTPMethod;
+		if(url.length == 0)
+			return;
 
 		runTask({
 			requestHTTP(url~_event,
@@ -25,8 +46,7 @@ public:
 				req.method = HTTPMethod.POST;
 				
 				req.writeBody(cast(ubyte[])"");
-			}
-			);
+			});
 		});
 	}
 }
