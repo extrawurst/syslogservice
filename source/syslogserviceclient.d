ae9361742cbe50d648fc52286b74910416e07640
Stephan Dilly <stephan@extrawurst.org>
2014-03-11 17:21:13 +0100

    simple syslog client lib to make the actul request to log something on the server side

---
+++ source/syslogserviceclient.d
@@ -0,0 +1,34 @@
+﻿module syslogserviceclient;
+
+///
+final class SyslogServiceClient
+{
+private:
+	immutable string url;
+
+public:
+	///
+	this(in string _url)
+	{
+		url = _url;
+	}
+
+	///
+	void log(string _event)()
+	{
+		import vibe.core.core:runTask;
+		import vibe.http.client:requestHTTP,HTTPClientRequest,HTTPMethod;
+
+		runTask({
+			requestHTTP(url~_event,
+			            (scope HTTPClientRequest req) {
+				req.method = HTTPMethod.POST;
+				
+				req.writeBody(cast(ubyte[])"");
+			}
+			);
+		});
+	}
+}
+
+
