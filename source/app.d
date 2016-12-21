002380cc652e7e82b8724526468e053276de7fe2
Stephan Dilly <dilly.stephan@gmail.com>
2016-12-21 02:22:59 +0100

    update to current vibe.d: 0.7.30

--- source/app.d
+++ source/app.d
@@ -13,12 +13,12 @@ shared static this()
 	bool quiet = true;
 	bool oneFilePerHour = false;
 
-	getOption("folder",&logFolder,"log folder (default './'");
-	getOption("hostport",&port,"port (default '8888')");
-	getOption("hostname",&hostName,"hostname (default 'hostUnknown'");
-	getOption("quiet",&quiet,"disable logging of each request to stdout (default 'true')");
-	getOption("filePerHour",&oneFilePerHour,"split log files per hour (default is split per day)");
-	getOption("file-suffix",&fileSuffix,"added to every log filename (default is '')");
+	readOption("folder",&logFolder,"log folder (default './'");
+	readOption("hostport",&port,"port (default '8888')");
+	readOption("hostname",&hostName,"hostname (default 'hostUnknown'");
+	readOption("quiet",&quiet,"disable logging of each request to stdout (default 'true')");
+	readOption("filePerHour",&oneFilePerHour,"split log files per hour (default is split per day)");
+	readOption("file-suffix",&fileSuffix,"added to every log filename (default is '')");
 
 	auto logger = new SysLogService();
 	logger.port = port;
